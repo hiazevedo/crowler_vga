f61597a02dfb6b38b61765d589f4bba1759438cf
higor.azevedo <higor.azevedo@somoseducacao.com.br>
2022-09-18 17:06:49 -0300

    extracao 18/09

--- vga_crowler.py
+++ vga_crowler.py
@@ -1,5 +1,6 @@
 import re
 import sqlite3
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import selenium.webdriver.chrome.service as service
@@ -12,7 +13,7 @@ def get_vga(service=None):
     op.add_argument('--disable-dev-shm-usage')
     op.add_argument("--remote-debugging-port=9222")
 
-    chromedriver_path = "/home/higor/Documentos/projetos/crowler_vga/webdriver/chromedriver"
+    chromedriver_path = "/home/higor/Documentos/higor/python/crowler_vga/webdriver/chromedriver"
 
     service = service.Service(chromedriver_path)
     service.start()
@@ -24,12 +25,15 @@ def get_vga(service=None):
         url = f'https://www.julianocaju.com.br/ofertas/placas-de-video.asp'
         driver.get(url)
 
+        sleep(5)
+
         lista = driver.find_elements(By.XPATH, '//*[@id="lista-ofertas"]/li/a')
 
         print('===== Coletando lista de VGA =====')
 
         for element in lista:
             link = element.get_attribute('href')
+            qtd_paragrafo = len(element.find_elements(By.XPATH, 'p'))
             nome = element.find_element(By.XPATH, 'p[1]').text
             modelo = element.find_element(By.XPATH, 'p[2]').text
 
@@ -41,13 +45,20 @@ def get_vga(service=None):
             r = r.replace(',', '.')
 
             valor = float(r)
+
+            indice_loja = 4
+
+            if qtd_paragrafo >= 6:
+                indice_loja = 5
+
             pcupomdes = element.find_elements(By.XPATH, 'p[4][@class="cupomdesc"]')
             if len(pcupomdes) == 0:
                 cupom_desconto = None
-                loja = element.find_element(By.XPATH, 'p[5]/span/img').get_attribute('alt')
+                loja = element.find_element(By.XPATH, 'p[' + str(indice_loja) + ']/span/img').get_attribute('alt')
             else:
+                indice_loja = indice_loja + 1
                 cupom_desconto = pcupomdes[0].text[19:]
-                loja = element.find_element(By.XPATH, 'p[6]/span/img').get_attribute('alt')
+                loja = element.find_element(By.XPATH, 'p[' + str(indice_loja) + ']/span/img').get_attribute('alt')
 
             dict_produtos.append({'nome': nome,
                                   'modelo': modelo,
@@ -56,6 +67,9 @@ def get_vga(service=None):
                                   'cupom_desconto': cupom_desconto,
                                   'link': link
                                   })
+
+
+            print(f'===== Coletando {modelo} =====')
     finally:
         driver.close()
         service.stop()
